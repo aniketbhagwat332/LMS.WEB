f5c1c7b1eb79077cbcc78b70f5917f22be72de27
aniketbhagwat332 <aniketbhagwat332@gmail.com>
2026-01-30 15:58:20 +0530

    added qa chnges for edit

--- LMS.Web/Controllers/CoursesController.cs
+++ LMS.Web/Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
 ﻿using LMS.Repository.Models;
+using LMS.Services.DTO;
 using LMS.Services.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -53,6 +54,52 @@ namespace LMS.Web.Controllers
             var courses = await _courseService.GetApprovedCoursesAsync();
             return View(courses);
         }
+        [HttpGet]
+        [Authorize(Roles = "Admin,Instructor")]
+        public async Task<IActionResult> Edit(int courseId)
+        {
+            int userId = int.Parse(User.FindFirst("UserId")!.Value);
+            string role = User.FindFirst(System.Security.Claims.ClaimTypes.Role)!.Value;
 
+            var course = await _courseService
+                .GetCourseDetailAsync(courseId, userId, role);
+
+            if (course == null)
+                return NotFound();
+
+            return View(new UpdateCourseDTO
+            {
+                CourseId = course.CourseId,
+                CourseTitle = course.CourseTitle,
+                Description = course.Description
+            });
+        }
+        [HttpPost]
+        [Authorize(Roles = "Admin,Instructor")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(UpdateCourseDTO model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            try
+            {
+                int userId = int.Parse(User.FindFirst("UserId")!.Value);
+                string role = User.FindFirst(System.Security.Claims.ClaimTypes.Role)!.Value;
+
+                await _courseService.UpdateCourseAsync(model, userId, role);
+
+                return RedirectToAction("Index");
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Forbid();
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View(model);
+            }
+        }
     }
 }
